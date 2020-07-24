d4515d31214061b4ba969efbbbad8abbe31a3945
Sara Sinback <sara.sinback@gmail.com>
2020-07-24 16:03:37 -0400

    python: make package
    Make a python package which provides the parts of apriltag which the
    dill repository uses.

---
+++ python/apriltag/__init__.py
@@ -0,0 +1,9 @@
+from .apriltag import Detection
+from .apriltag import Detector
+from .apriltag import _get_demo_searchpath
+
+__all__ = [
+    'Detection',
+    'Detector',
+    '_get_demo_searchpath',
+]
---
+++ python/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='apriltag',
+    version='1.0.0',
+    author='sinback',
+    author_email='sinback@picklerobot.com',
+    description='Apriltag Python utilities',
+    long_description='Forked from https://github.com/swatbotics/apriltag',
+    url='https://github.com/Pickle-Robot/apriltag',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: The Regents of the University of Michigan',
+        'Operating System :: Linux',
+    ],
+    python_requires='>=3.0',
+)
